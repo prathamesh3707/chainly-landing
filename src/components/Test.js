6005684a0509bb752ce876be9769cf96ebd155f1
prathamesh3707 <prathameshk3707@gmail.com>
2023-10-01 01:58:08 +0530

    tailwind

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,7 +1,79 @@
 import React from "react";
 
 function Test() {
-	return <div className="text-green-600">Test</div>;
+	return (
+		<div className="flex flex-row justify-evenly w-4/5 mx-auto">
+			<div className="border w-72 border-brand rounded-lg h-3/5 p-2 my-8 bg-gradient-to-b from-back to-black">
+				<div className="w-3/5 mx-auto h-12 -mt-8 py-3 rounded-2xl bg-gradient-to-r from-brand1 to-brand">
+					INCUBATION
+				</div>
+				<div className="border-b border-borbot mt-4 p-2">
+					Access Top Incubators
+				</div>
+				<div className="border-b border-borbot mt-3 p-2">Reputed Callers</div>
+				<div className="border-b border-borbot mt-3 p-2">
+					Better growth Advice
+				</div>
+				<div className="border-b border-borbot mt-3 p-2">
+					Amazing discounted deals
+				</div>
+				<div className="border-b border-borbot mt-3 p-2">
+					Private group membership
+				</div>
+				<div className="border-b border-borbot mt-3 p-2">
+					Fund raising strategy
+				</div>
+				<div className="border-b border-borbot mt-3 p-2">Demo day</div>
+				<div className="w-3/5 mx-auto h-12 text-white mt-6 mb-3 py-3 rounded-2xl bg-gradient-to-r from-brand1 to-brand">
+					GET IN TOUCH
+				</div>
+			</div>
+			<div className="border w-72 border-brand rounded-lg h-3/5 p-2 my-8 bg-gradient-to-b from-back to-black">
+				<div className="w-3/5 mx-auto h-12 -mt-8 py-3 rounded-2xl bg-gradient-to-r from-brand1 to-brand">
+					MARKETING
+				</div>
+				<div className="border-b border-borbot mt-4 p-2">24/7 Active Mods</div>
+				<div className="border-b border-borbot mt-3 p-2">
+					Community Management
+				</div>
+				<div className="border-b border-borbot mt-3 p-2">Press Release</div>
+				<div className="border-b border-borbot mt-3 p-2">Shilling</div>
+				<div className="border-b border-borbot mt-3 p-2">
+					Marketing Strategy
+				</div>
+				<div className="border-b border-borbot mt-3 p-2">AMAs/ KOLS</div>
+				<div className="border-b border-borbot mt-3 p-2">Billboards</div>
+				<div className="w-3/5 mx-auto h-12 text-white mt-6 mb-3 py-3 rounded-2xl bg-gradient-to-r from-brand1 to-brand">
+					GET IN TOUCH
+				</div>
+			</div>
+			<div className="border w-72 border-brand rounded-lg h-3/5 p-2 my-8 bg-gradient-to-b from-back to-black">
+				<div className="w-3/5 mx-auto h-12 -mt-8 py-3 rounded-2xl bg-gradient-to-r from-brand1 to-brand">
+					LISTING
+				</div>
+				<div className="border-b border-borbot mt-4 p-2">
+					Access Top Incubators
+				</div>
+				<div className="border-b border-borbot mt-3 p-2">Reputed Callers</div>
+				<div className="border-b border-borbot mt-3 p-2">
+					Better growth Advice
+				</div>
+				<div className="border-b border-borbot mt-3 p-2">
+					Amazing discounted deals
+				</div>
+				<div className="border-b border-borbot mt-3 p-2">
+					Private group membership
+				</div>
+				<div className="border-b border-borbot mt-3 p-2">
+					Fund raising strategy
+				</div>
+				<div className="border-b border-borbot mt-3 p-2">Demo day</div>
+				<div className="w-3/5 mx-auto h-12 text-white mt-6 mb-3 py-3 rounded-2xl bg-gradient-to-r from-brand1 to-brand">
+					GET IN TOUCH
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Test;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
 	content: ["./src/**/*.{html,js}"],
 	theme: {
+		colors: {
+			brand: "#0169F0",
+			brand1: "#01C2FF",
+			back: "#0E2B83",
+			borbot: "#1972E2",
+		},
+
 		extend: {},
 	},
 	plugins: [],
