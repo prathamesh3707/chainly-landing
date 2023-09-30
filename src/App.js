9db52252ac3b5e3f7881435314cf197b46ba4fea
prathamesh3707 <prathameshk3707@gmail.com>
2023-09-30 23:57:53 +0530

    font added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Hero from "./components/Hero";
+import Second from "./components/Second";
 
 function App() {
   return (
     <div className="App">
       <Hero />
+      <Second />
     </div>
   );
 }
---
+++ src/components/Second.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Second.css";
+
+const Second = () => {
+  return (
+    <div className="second-container">
+      <h1 className="second-heading">
+        At Chainly, we've curated a suite of services designed to empower your
+        web3 ventures.
+      </h1>
+      <p className="second-content">
+        Our offerings extend far beyond the conventional, bringing together the
+        essential elements that shape success in the decentralized landscape.
+        <br />
+        <br />
+        Our journey together is defined by collaboration, where your vision
+        merges seamlessly with our expertise to transcend what's possible and
+        usher in a new era of web3 marvels.
+        <br />
+        <br />
+        Our mission is to propel the creative evolution of decentralized
+        ecosystems, transforming them into thriving landscapes of innovation and
+        opportunity.
+      </p>
+    </div>
+  );
+};
+
+export default Second;
