7805741d26af0a6da46c7488aeff5a06021342fd
prathamesh3707 <prathameshk3707@gmail.com>
2023-10-01 00:05:12 +0530

    tailwind

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Hero from "./components/Hero";
+import Test from "./components/Test";
 
 function App() {
-  return (
-    <div className="App">
-      <Hero />
-    </div>
-  );
+	return (
+		<div className="App">
+			<Hero />
+			<Test />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Test.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Test() {
+	return <div className="text-green-600">Test</div>;
+}
+
+export default Test;
