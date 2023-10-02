150d8eb9e407693eb4f97c31cf99d259c92e819b
prathamesh3707 <prathameshk3707@gmail.com>
2023-10-02 20:55:49 +0530

    done

--- src/components/Test/index.js
+++ src/components/Test/index.js
@@ -16,19 +16,18 @@ function Test() {
 				<br />
 				<br />
 			</div>
+
 			<div className={styles.rightImage}></div>
 			<div className={styles.leftImage}></div>
 
 			{/* Boxes */}
 			<div
-				className="flex flex-col md:flex-row justify-evenly w-5/6 md:w-3/4 mx-auto"
-				style={{
-					background: "#000211",
-					width: "100%",
-					paddingBottom: "5rem",
-				}}
+				className="flex flex-col md:flex-row justify-evenly w-5/6  mx-auto"
+				style={{ background: "#000211", width: "100%", paddingBottom: "5rem" }}
 			>
-				<div className="border w-96 border-brand rounded-lg h-3/5 p-2 my-8 bg-gradient-to-b from-back to-black">
+				<div className={styles.rightImage}></div>
+
+				<div className="md:mx-auto border w-96 border-brand rounded-lg h-3/5 p-2 my-8 bg-gradient-to-b from-back to-black">
 					<div
 						className="w-3/5 mx-auto h-12 -mt-10 py-3 rounded-2xl bg-brand flex"
 						style={{ fontFamily: "Gilroy-Bold", color: "white" }}
@@ -68,7 +67,7 @@ function Test() {
 						</div>
 					</div>
 				</div>
-				<div className="border w-96 border-brand rounded-lg h-3/5 p-2 my-8 bg-gradient-to-b from-back to-black">
+				<div className="md:mx-auto border w-96 border-brand rounded-lg h-3/5 p-2 my-8 bg-gradient-to-b from-back to-black">
 					<div
 						className="w-3/5 mx-auto h-12 -mt-10 py-3 rounded-2xl bg-brand flex"
 						style={{ fontFamily: "Gilroy-Bold", color: "white" }}
@@ -77,26 +76,26 @@ function Test() {
 					</div>
 					<div style={{ color: "white", fontFamily: "Gilroy" }}>
 						<div className="border-b border-borbot mt-4 p-2 flex">
-							<div className="mx-auto">Access Top Incubators</div>
+							<div className="mx-auto">24/7 Active Mods</div>
 						</div>
 						<div className="border-b border-borbot mt-3 p-2 flex">
 							{" "}
-							<div className="mx-auto">Reputed Callers</div>
+							<div className="mx-auto">Community Management</div>
 						</div>
 						<div className="border-b border-borbot mt-3 p-2 flex">
-							<div className="mx-auto">Better growth Advice</div>
+							<div className="mx-auto">Press Release</div>
 						</div>
 						<div className="border-b border-borbot mt-3 p-2 flex">
-							<div className="mx-auto">Amazing discounted deals</div>
+							<div className="mx-auto">Shilling</div>
 						</div>
 						<div className="border-b border-borbot mt-3 p-2 flex">
-							<div className="mx-auto"> Private group membership</div>
+							<div className="mx-auto">Marketing strategy</div>
 						</div>
 						<div className="border-b border-borbot mt-3 p-2 flex">
-							<div className="mx-auto">Fund raising strategy</div>
+							<div className="mx-auto">AMAs/KOLs</div>
 						</div>
 						<div className="border-b border-borbot mt-3 p-2 flex">
-							<div className="mx-auto"> Demo day</div>
+							<div className="mx-auto">Billboards</div>
 						</div>
 					</div>
 					<div className="w-3/5 mx-auto h-12 text-white mt-6 mb-3 py-3 rounded-2xl bg-gradient-to-r from-brand1 to-brand flex">
@@ -108,7 +107,7 @@ function Test() {
 						</div>
 					</div>
 				</div>
-				<div className="border w-96 border-brand rounded-lg h-3/5 p-2 my-8 bg-gradient-to-b from-back to-black">
+				<div className="md:mx-auto border w-96 border-brand rounded-lg h-3/5 p-2 my-8 bg-gradient-to-b from-back to-black">
 					<div
 						className="w-3/5 mx-auto h-12 -mt-10 py-3 rounded-2xl bg-brand flex"
 						style={{ fontFamily: "Gilroy-Bold", color: "white" }}
@@ -152,5 +151,4 @@ function Test() {
 		</div>
 	);
 }
-
 export default Test;
