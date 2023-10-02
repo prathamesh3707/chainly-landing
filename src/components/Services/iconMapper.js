373365619cd4aa861d8169c67ad13972333d281e
prathamesh3707 <prathameshk3707@gmail.com>
2023-10-02 12:53:16 +0530

    done

--- src/components/Services/iconMapper.js
+++ src/components/Services/iconMapper.js
@@ -2,7 +2,7 @@ import { ReactComponent as Paper } from "../../assets/icons/paper.svg";
 import { ReactComponent as Agile } from "../../assets/icons/Agile.svg";
 import { ReactComponent as Analysis } from "../../assets/icons/analysis 1.svg";
 import { ReactComponent as Candles } from "../../assets/icons/candles.svg";
-import { ReactComponent as Design } from "../../assets/icons/Design.svg";
+import { ReactComponent as Design } from "../../assets/icons/design.svg";
 import { ReactComponent as Document } from "../../assets/icons/document 1.svg";
 import { ReactComponent as Funding } from "../../assets/icons/Funding.svg";
 import { ReactComponent as Handshake } from "../../assets/icons/handshake 1.svg";
@@ -14,20 +14,20 @@ import { ReactComponent as Party } from "../../assets/icons/Party.svg";
 import { ReactComponent as Programming } from "../../assets/icons/web-programming 1.svg";
 
 const iconMapper = {
-  paper: Paper,
-  agile: Agile,
-  analysis: Analysis,
-  candles: Candles,
-  design: Design,
-  document: Document,
-  funding: Funding,
-  handshake: Handshake,
-  marketing: Marketing,
-  pieChart: PieChart,
-  pie: Pie,
-  product: Product,
-  party: Party,
-  programming: Programming,
+	paper: Paper,
+	agile: Agile,
+	analysis: Analysis,
+	candles: Candles,
+	design: Design,
+	document: Document,
+	funding: Funding,
+	handshake: Handshake,
+	marketing: Marketing,
+	pieChart: PieChart,
+	pie: Pie,
+	product: Product,
+	party: Party,
+	programming: Programming,
 };
 
 export default iconMapper;
