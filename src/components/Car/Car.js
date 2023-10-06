b63c237d1ca8fd2810beff96f1c98428798e9a94
aryabyte21 <arya2001bhosale@gmail.com>
2023-10-06 18:55:38 +0530

    carousel

--- src/components/Car/Car.js
+++ src/components/Car/Car.js
@@ -1,37 +1,47 @@
 import React, { useState, useEffect } from "react";
 import styles from "./Car.module.css";
-
+import partner1 from "../../assets/Partners2.svg";
+import partner2 from "../../assets/Partners.svg";
+import partner3 from "../../assets/Partners3.svg";
+import Title from "../Title";
 const Car = () => {
-	const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-	const images = [
-		"https://cdni.autocarindia.com/utils/imageresizer.ashx?n=https://cms.haymarketindia.net/model/uploads/modelimages/Hyundai-Grand-i10-Nios-200120231541.jpg",
-		"../../assets/Partners2.svg",
-		"../../assets/Partners2.svg",
-	];
+  const images = [partner3, partner1, partner2];
 
-	useEffect(() => {
-		const timer = setTimeout(() => {
-			setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-		}, 3000); // Change image every 3 seconds
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 3000);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [currentIndex, images.length]);
 
-		return () => {
-			clearTimeout(timer);
-		};
-	}, [currentIndex, images.length]);
-
-	return (
-		<div className={styles.carousel}>
-			{images.map((image, index) => (
-				<img
-					key={index}
-					src={image}
-					alt={`Slide ${index}`}
-					className={index === currentIndex ? styles.active : styles.slide}
-				/>
-			))}
-		</div>
-	);
+  return (
+    <div>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          marginTop: "4rem",
+        }}
+      >
+        <Title titleName="Chainly Ecosystem 🛸" />
+      </div>
+      <div className={styles.carousel}>
+        {images.map((image, index) => (
+          <img
+            key={index}
+            src={image}
+            alt={`Slide ${index}`}
+            className={index === currentIndex ? styles.active : styles.slide}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Car;
