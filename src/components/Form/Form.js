84ca1b3d7ae92853b9095822e835eade8ecba9bd
prathamesh3707 <prathameshk3707@gmail.com>
2023-10-08 00:19:15 +0530

    done

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -61,7 +61,7 @@ function Form() {
 					</div>
 					<button
 						type="submit"
-						class="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+						class="py-3 px-5 text-sm w-2/5 font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
 					>
 						Send message
 					</button>
