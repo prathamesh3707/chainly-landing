6f085004f2204839cf36bc33a7649ff32b399824
prathamesh3707 <prathameshk3707@gmail.com>
2023-10-08 00:11:22 +0530

    done

--- src/components/Navabar/index.js
+++ src/components/Navabar/index.js
@@ -1,17 +1,41 @@
-import React from "react";
 import styles from "./Navbar.module.css";
 import logo from "../../assets/Chainly.png"; // Adjust the path based on your directory structure
-
+import React, { useState } from "react";
 function Navbar() {
-  return (
-    <div className={styles.navbar}>
-      <img src={logo} alt="Logo" className={styles.logo} />
-      <a href="#home">Home</a>
-      <a href="#about-us">About Us</a>
-      <a href="#services">Services</a>
-      <a href="#pricing">Pricing</a>
-    </div>
-  );
+	const [isOpen, setIsOpen] = useState(false);
+	return (
+		<div>
+			<div className={styles.navbar}>
+				<img src={logo} alt="Logo" className={styles.logo} />
+				<a href="#home">Home</a>
+				<a href="#about-us">About Us</a>
+				<a href="#services">Services</a>
+				<a href="#pricing">Pricing</a>
+			</div>
+
+			<div className={styles.dropdown_navbar}>
+				{!isOpen && (
+					<button className={styles.iconic} onClick={() => setIsOpen(!isOpen)}>
+						☰
+					</button>
+				)}
+				{isOpen && (
+					<button className={styles.iconic} onClick={() => setIsOpen(!isOpen)}>
+						✖
+					</button>
+				)}
+
+				{isOpen && (
+					<div className={styles.menu}>
+						<a href="#home">Home</a>
+						<a href="#about-us">About Us</a>
+						<a href="#services">Services</a>
+						<a href="#pricing">Pricing</a>
+					</div>
+				)}
+			</div>
+		</div>
+	);
 }
 
 export default Navbar;
