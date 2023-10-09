df2ad0c7718806e4419f99b369d1789234f90627
prathamesh3707 <prathameshk3707@gmail.com>
2023-10-09 19:31:18 +0530

    done

--- src/components/Connect/index.js
+++ src/components/Connect/index.js
@@ -1,34 +1,35 @@
 import React, { useEffect } from "react";
-import "./Connect.css";
+import styles from "./Connect.module.css";
+import chainly from "../../assets/calendly.webp";
 
 function CalendlyButton() {
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://assets.calendly.com/assets/external/widget.js";
-    script.async = true;
-    document.body.appendChild(script);
+	useEffect(() => {
+		const script = document.createElement("script");
+		script.src = "https://assets.calendly.com/assets/external/widget.js";
+		script.async = true;
+		document.body.appendChild(script);
 
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
+		return () => {
+			document.body.removeChild(script);
+		};
+	}, []);
 
-  const openCalendly = () => {
-    if (window.Calendly) {
-      window.Calendly.initPopupWidget({
-        url: "https://calendly.com/chainlystudio/consult",
-      });
-    }
-    return false;
-  };
+	const openCalendly = () => {
+		if (window.Calendly) {
+			window.Calendly.initPopupWidget({
+				url: "https://calendly.com/chainlystudio/consult",
+			});
+		}
+		return false;
+	};
 
-  return (
-    <div className="calendly-container">
-      <button className="calendly-button" onClick={openCalendly}>
-        LET'S TALK
-      </button>
-    </div>
-  );
+	return (
+		<div className={styles.calendly_container}>
+			<button className={styles.but} onClick={openCalendly}>
+				<img className={styles.chain} src={chainly} alt="hello" />
+			</button>
+		</div>
+	);
 }
 
 export default CalendlyButton;
