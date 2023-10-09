b0edde658fa92842eb280be877e7a102034c5b9b
aryabyte21 <arya2001bhosale@gmail.com>
2023-10-09 14:36:59 +0530

    done

--- src/components/Navabar/index.js
+++ src/components/Navabar/index.js
@@ -1,46 +1,62 @@
 import styles from "./Navbar.module.css";
 import logo from "../../assets/Chainly.png"; // Adjust the path based on your directory structure
 import nav from "../../assets/nav.png"; // Adjust the path based on your directory structure
-import CalendlyButton from "../Connect/Connect";
+// import CalendlyButton from "../Connect/Connect";
+import { useEffect, useRef } from "react";
 import React, { useState } from "react";
 function Navbar() {
-	const [isOpen, setIsOpen] = useState(false);
-	return (
-		<div>
-			<div className={styles.navbar}>
-				<img src={logo} alt="Logo" className={styles.logo} />
-				<a href="#home">Home</a>
-				<a href="#about-us">About Us</a>
-				<a href="#services">Services</a>
-				<a href="#pricing">Pricing</a>
-				<a href="https://calendly.com/chainlystudio/consult">
+  const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef(null);
+
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <div>
+      <div className={styles.navbar}>
+        <img src={logo} alt="Logo" className={styles.logo} />
+        <a href="#home">Home</a>
+        <a href="#about-us">About Us</a>
+        <a href="#services">Services</a>
+        <a href="#pricing">Pricing</a>
+        {/* <a href="https://calendly.com/chainlystudio/consult">
 					<CalendlyButton />{" "}
-				</a>
-			</div>
+				</a> */}
+      </div>
 
-			<div className={styles.dropdown_navbar}>
-				{!isOpen && (
-					<button className={styles.iconic} onClick={() => setIsOpen(!isOpen)}>
-						<img src={nav} alt="hello" className={styles.image} />
-					</button>
-				)}
-				{isOpen && (
-					<button className={styles.iconic} onClick={() => setIsOpen(!isOpen)}>
-						✘
-					</button>
-				)}
+      <div className={styles.dropdown_navbar}>
+        {!isOpen && (
+          <button className={styles.iconic} onClick={() => setIsOpen(!isOpen)}>
+            <img src={nav} alt="hello" className={styles.image} />
+          </button>
+        )}
+        {isOpen && (
+          <button className={styles.iconic} onClick={() => setIsOpen(!isOpen)}>
+            ✘
+          </button>
+        )}
 
-				{isOpen && (
-					<div className={styles.menu}>
-						<a href="#home">Home</a>
-						<a href="#about-us">About Us</a>
-						<a href="#services">Services</a>
-						<a href="#pricing">Pricing</a>
-					</div>
-				)}
-			</div>
-		</div>
-	);
+        {isOpen && (
+          <div className={styles.menu} ref={dropdownRef}>
+            <a href="#home">Home</a>
+            <a href="#about-us">About Us</a>
+            <a href="#services">Services</a>
+            <a href="#pricing">Pricing</a>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
